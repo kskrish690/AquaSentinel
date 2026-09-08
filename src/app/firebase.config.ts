ebfee5a672a3277233035e0915fbae2eb6bb469c
kskrish690 <kskrish690@gmail.com>
2026-09-08 12:06:08 +0530

    Fix Firebase project configuration

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyCQvCNUaJnX8IhyBHRJSbMZDDVIk0ccEq0",
-  authDomain: "AquaSentinal-5b215.firebaseapp.com",
-  projectId: "AquaSentinal-5b215",
-  storageBucket: "AquaSentinal-5b215.firebasestorage.app",
+  authDomain: "AquaSentinel-5b215.firebaseapp.com",
+  projectId: "AquaSentinel-5b215",
+  storageBucket: "AquaSentinel-5b215.firebasestorage.app",
   messagingSenderId: "419886773980",
   appId: "1:419886773980:web:bb1909925cbc1e49712619",
   measurementId: "G-N18J9RCE08"
